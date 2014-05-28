36c8df5cd0b47540d4cf2a0af081b83ebab33f06
Taylor Potter <taylorkpotter@me.com>
2014-05-28 02:16:20 -0700

    Added README

---
+++ README.md
@@ -0,0 +1,10 @@
+##Class Roster
+
+![alt tag](RosterApp/ss.png)
+=====
+
+#### This was the first app we created during the CodeFellows iOS Accelerator. An easy way to store classmates names and contact information. 
+
+- Data persistancy using .plist
+- Share Sheet set up for twitter
+- RGB slider for background color
--- RosterApp/PersonViewController.m
+++ RosterApp/PersonViewController.m
@@ -28,14 +28,7 @@
 
 @implementation PersonViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+
 
 - (void)viewDidLoad
 {
